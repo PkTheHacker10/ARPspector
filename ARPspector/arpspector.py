4d1f947612043558c8cfa647a68ef87cc917c4dc
pk <pk_server@pkserver@quickcom.com>
2025-01-03 16:40:33 +0000

    setup file updated

---
+++ ARPspector/arpspector.py
@@ -0,0 +1,11 @@
+from colorama import Fore,Style 
+
+red=Fore.RED
+blue=Fore.BLUE
+bright=Style.BRIGHT
+white=Fore.WHITE
+reset=Style.RESET_ALL
+
+def main():
+    pass
+
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+
+with open("README.md","r",encoding="utf-8") as file:
+    tool_description=file.read()
+    
+setup(
+    name="ARPspector",
+    version='1.0.0',
+    author="pk_the_hacker",
+    author_email="pevinbalaji@gmail.com",
+    description=tool_description,
+    packages=find_packages(),
+    install_requires=[
+        'art==6.4',
+        'colorama==0.4.6',
+        'scapy>=2.6.1',
+        'requests>=2.32.3'
+    ],
+    entry_points={
+        "console_scripts":[
+            "arpspector=ARPspector.arpspector:main"
+        ],
+    }
+
+)

361d41f656a0fbcb6c1d5bbd52bbacdf48c8a4ff
pk <pk_server@pkserver@quickcom.com>
2025-01-04 15:55:50 +0000

    monitor started

--- ARPspector/modules/inspector/monitor.py
+++ ARPspector/modules/inspector/monitor.py
@@ -1,4 +1,6 @@
+from time import sleep
 from scapy.all import *
+from threading import Thread,ThreadError,Lock
 from colorama import Fore,Style 
 
 red=Fore.RED
@@ -7,16 +9,37 @@ bright=Style.BRIGHT
 white=Fore.WHITE
 reset=Style.RESET_ALL
 
+arp_packet=[]
+
 class ArpInspector():
     
     def sniffer(self):
-        pass
-    
-    def analyzer(self):
-        pass
+        while True:
+            try:
+                Lock.acquire()
+                arp_packet.append(sniff(filter="arp")[0])
+                
+            except Exception as LockError:
+                print(f" {bright}{red}[ + ] Sniffing Error :{reset} {LockError}")
+                
+            finally:
+                Lock.release()
+            
     
     def logger(self):
         pass
     
-    
+    def inspector_handler(self):
+        try:
+            sniffer_thread=Thread(target=self.sniffer(),args=())
+            sniffer_thread.start()
+            
+        except ThreadError as ThreadingError:
+            print(f" {bright}{red}[ + ] Thread Error :{reset} {ThreadingError}")
+            
+        
+            
+if __name__=="__main__":
+    ai=ArpInspector()
+    ai.sniffer()
     

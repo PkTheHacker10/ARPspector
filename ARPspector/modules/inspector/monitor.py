1cb20734a87d9578ff63609dbc0a3eefde720e19
root <root@pkserver@quickcom.com>
2025-01-06 16:50:49 +0000

    arp single packet add bug

--- ARPspector/modules/inspector/monitor.py
+++ ARPspector/modules/inspector/monitor.py
@@ -18,32 +18,34 @@ class ArpInspector():
     
     def sniffer(self):
         try:
-            print("inside sniffer")
+            print("sniffer started")
+            global arp_packet
             while True:
-                global arp_packet
                 packet=sniff(filter="arp",count=1)
                 arp=packet[0]
                 if ARP in arp:
-                    arp_packet["arp_replay"]={
-                        "src_ip":arp.psrc,
-                        "src_mac":arp.hwdst,
-                        "dst_ip":arp.pdst,
-                        "dst_mac":arp.hwdst
-                        }
-                print(arp_packet)
+                    with lock:
+                        arp_packet["arp_replay"]={
+                            "src_ip":arp.psrc,
+                            "src_mac":arp.hwdst,
+                            "dst_ip":arp.pdst,
+                            "dst_mac":arp.hwdst
+                            }
                    
         except Exception as SnifferError:
             print(f"{bright}{red}[ + ] Error on sniffer: {SnifferError}{reset}")
             
     def inspector_handler(self):
         try:
+            global arp_packet
             sniffer_thread=Thread(target=self.sniffer,args=())
             sniffer_thread.start()
-            
-            for i in range(1,10):
+            while True:
                 print(arp_packet)
-                print("sleeping 2 sec..")
-                sleep(2)
+                print(f"Length : {len(arp_packet)}")
+                print("------------------------------------------")
+                print("sleeping 0.6 sec..")
+                sleep(0.6)
             
         except Exception as InspectorHandlerError:
             print(f" {bright}{red}[ + ] Thread Error :{reset} {InspectorHandlerError}")

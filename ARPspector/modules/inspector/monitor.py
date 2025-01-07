219d11a88bc0f9517fc1944e8a28e6f6e8d0018e
pk <pk_server@pkserver@quickcom.com>
2025-01-07 03:54:15 +0000

    arp packet sniffing done

--- ARPspector/modules/inspector/monitor.py
+++ ARPspector/modules/inspector/monitor.py
@@ -20,17 +20,19 @@ class ArpInspector():
         try:
             print("sniffer started")
             global arp_packet
+            i=1
             while True:
                 packet=sniff(filter="arp",count=1)
                 arp=packet[0]
                 if ARP in arp:
                     with lock:
-                        arp_packet["arp_replay"]={
+                        arp_packet[f"arp_replay: {i}"]={
                             "src_ip":arp.psrc,
                             "src_mac":arp.hwdst,
                             "dst_ip":arp.pdst,
                             "dst_mac":arp.hwdst
                             }
+                    i=i+1
                    
         except Exception as SnifferError:
             print(f"{bright}{red}[ + ] Error on sniffer: {SnifferError}{reset}")
--- ARPspector/modules/inspector/test.py
+++ ARPspector/modules/inspector/test.py
@@ -1,7 +1,10 @@
 from scapy.all import *
-
-while True:
+i=1
+for _ in range(1,11):
     ether_frame=Ether(dst="ff:ff:ff:ff:ff:ff")
     arp=ARP(pdst="192.168.53.156")
     packet=ether_frame/arp
-    ans=sendp(packet)
+    ans=sendp(packet,verbose=False)
+    print(f"Sented packet: {i}")
+    i=i+1
+    
